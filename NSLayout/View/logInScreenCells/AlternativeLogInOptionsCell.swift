e880cebb408ff63e6b02843d1fc7e8bb403d9de5
RamyZayed <rami1112@windowslive.com>
2022-05-12 10:18:57 +0300

    New login Screen using tableview a back button to the info screen

---
+++ NSLayout/View/logInScreenCells/AlternativeLogInOptionsCell.swift
@@ -0,0 +1,23 @@
+//
+//  AlternativeLogInOptionsCell.swift
+//  NSLayout
+//
+//  Created by Mobile Team on 11/05/2022.
+//
+
+import UIKit
+
+class AlternativeLogInOptionsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ NSLayout/View/logInScreenCells/RegisterCell.swift
@@ -0,0 +1,23 @@
+//
+//  RegisterCell.swift
+//  NSLayout
+//
+//  Created by Mobile Team on 11/05/2022.
+//
+
+import UIKit
+
+class RegisterCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
